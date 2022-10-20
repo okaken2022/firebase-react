b89732be0c2608130ba24045ff5831a1307418a6
okaken2022 <okaken.address@gmail.com>
2022-10-20 22:15:30 +0900

    add CRUD

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import { FormControl, List, TextField } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { db } from "./firebase"
+import AddToPhotosIcon from "@material-ui/icons/AddToPhotos"
+import { EventBusy } from '@material-ui/icons';
+import TaskItem from './TaskItem';
 
-function App() {
+const App: React.FC = () => {
+  const [tasks, setTasks] = useState([{id: "", title: ""}]);
+  const [input, setInput] = useState("");
+
+  useEffect(()=> {
+    const unSub  = db.collection("tasks").onSnapshot((snapshot)=>{
+      setTasks(
+        snapshot.docs.map((doc) => ({id: doc.id, title: doc.data().title}))
+      );
+    });
+    return () => unSub();
+  }, []);
+
+  const newTask = (e: React.MouseEvent<HTMLButtonElement>) => {
+    db.collection("tasks").add({title: input});
+    setInput("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo App by React/Firebase</h1>
+      <FormControl>
+        <TextField
+          InputLabelProps={{
+            shrink: true,
+          }}
+          label="New task ?"
+          value={input}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setInput(e.target.value)}
+        />
+      </FormControl>
+      <button disabled={!input}
+          onClick={newTask}>
+          <AddToPhotosIcon />
+      </button>
+      
+      <List>
+        {tasks.map((task)=> (<TaskItem key={task.id} id={task.id} title={task.title}/>
+        ))}
+      </List>
     </div>
   );
 }
