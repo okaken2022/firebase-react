02894a9731eacc69859c206eb6ffc66d58d3ccbb
okaken2022 <okaken.address@gmail.com>
2022-10-20 23:07:50 +0900

    add React-Router-DOM

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Login from "./Login";
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App/>}/>
+        <Route path="/Login" element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
