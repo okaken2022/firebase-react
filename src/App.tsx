967dcee3423503c40a4ef76b72c1723dd405724b
okaken2022 <okaken.address@gmail.com>
2022-10-23 23:59:05 +0900

    login機能追加

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,10 @@ import { EventBusy } from '@material-ui/icons';
 import { makeStyles } from '@material-ui/styles';
 import TaskItem from './TaskItem';
 
+import { auth } from "./firebase"
+import ExitToAppIcon from "@material-ui/icons/ExitToApp"
+
+
 const useStyles = makeStyles({
   field: {
     marginTop: 30,
@@ -18,10 +22,17 @@ const useStyles = makeStyles({
   },
 });
 
-const App: React.FC = () => {
+const App: React.FC = (props: any) => {
   const [tasks, setTasks] = useState([{id: "", title: ""}]);
   const [input, setInput] = useState("");
   const classes = useStyles();
+
+  useEffect(() => {
+    const unSub = auth.onAuthStateChanged((user)=>{
+      !user && props.history.push("login")
+    });
+    return ()=> unSub();
+  })
 
   useEffect(()=> {
     const unSub  = db.collection("tasks").onSnapshot((snapshot)=>{
@@ -40,6 +51,18 @@ const App: React.FC = () => {
   return (
     <div className={styles.app__root}>
       <h1>Todo App by React/Firebase</h1>
+      <button className={styles.app__logout}
+      onClick={
+        async () => {
+          try {await auth.signOut();
+          props.history.push("login");
+        } catch(error: any) {
+          alert(error.message)
+        }
+      }}
+      >
+        <ExitToAppIcon />
+      </button>
       <br />
       <FormControl>
         <TextField
